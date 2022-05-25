ab017fccee9c4d50861a8022dd9c779b9076952c
henrydufloth <henrydufloth@outlook.com>
2022-05-24 20:20:14 -0500

    Create

---
+++ HenryVDjeep-sales/src/main/java/com/promineotech/jeep/service/DefaultJeepSalesService.java
@@ -0,0 +1,26 @@
+
+   
+package com.promineotech.jeep.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Service
+@Slf4j
+public class DefaultJeepSalesService implements JeepSalesService {
+
+  @Override
+  public List<Jeep> fetchJeeps(JeepModel model, String trim) {
+	  log.info("Service fetch Jeeps model={}, trim={}", model, trim);
+    return null;
+  }
+
+}
